4163aeaca43e979185c99ca4f8e2f0f4e1f54884
maryam jafari <maryamjafari12175@gmail.com>
2024-09-11 10:34:01 +0330

    -new2

--- main.js
+++ main.js
@@ -58,7 +58,8 @@ function renderBaseInfo(infoItem){
 
   let labelBaseInfo = document.createElement("div");
   labelBaseInfo.classList.add("label");
-  labelBaseInfo.textContent= infoItem.id;
+  let final= (infoItem.id).toUpperCase();
+  labelBaseInfo.textContent= final;
   baseInfo.appendChild(labelBaseInfo);
 
   let numberBaseInfo = document.createElement("span");
@@ -88,7 +89,7 @@ function renderAssetsList (){
     theadTable.classList.add ("theadassetstable");
     assetsTable.appendChild(theadTable);
 
-    let assetsTableTr = document.createElement("tr");
+    let assetsTableTr = document.createElement("th");
     assetsTableTr.classList.add("theadtr");
     theadTable.appendChild(assetsTableTr);
 
@@ -105,7 +106,7 @@ function renderAssetsList (){
    tbodyTable.classList.add ("tbodyClass");
    assetsTable.appendChild(tbodyTable);
 
-   function tableTrTd(tdRank , tdName , tdPrice , tdMarket ,tdVWAP ,tdSupply ,tdVolume  , tdChange){
+   function tableTrTd(tdRank ,symbol, imglogo , tdName , tdPrice , tdMarket ,tdVWAP ,tdSupply ,tdVolume  , tdChange){
     let assetsTbodyTableTr = document.createElement("tr");
     assetsTbodyTableTr.classList.add("tbodytr");
     tbodyTable.appendChild(assetsTbodyTableTr);
@@ -116,9 +117,27 @@ function renderAssetsList (){
     TbodyTdRank.textContent=tdRank;
 
     let TbodyTdName = document.createElement("td");
-    TbodyTdName.classList.add("Tbodytd");
+    TbodyTdName.classList.add("Tbodytd1");
     assetsTbodyTableTr.appendChild(TbodyTdName);
-    TbodyTdName.textContent=tdName;
+    let tbodyTdimg = document.createElement("img");
+    tbodyTdimg.classList.add("tdimg");
+    TbodyTdName.appendChild(tbodyTdimg);
+    tbodyTdimg.src=imglogo;
+
+    let tbodytdname2 = document.createElement("a");
+    tbodytdname2.classList.add("tbodyname2");
+    TbodyTdName.appendChild(tbodytdname2);
+
+    let tbodyname3 = document.createElement("p");
+    tbodyname3.classList.add("smallname");
+    tbodytdname2.appendChild(tbodyname3);
+    tbodyname3.textContent=tdName;
+
+    let tbodyname4 = document.createElement("p");
+    tbodyname4.classList.add("smallname2");
+    tbodytdname2.appendChild(tbodyname4);
+    tbodyname4.textContent=symbol;
+
 
     function formatNumber(num) {
       if (num >= 1e12) {
@@ -169,7 +188,9 @@ function renderAssetsList (){
     let listofTrTd = await getAssetslist();
   for (let i=0 ; i<listofTrTd.length ; i++){
     let itemofTrTd = listofTrTd[i];
-     tableTrTd(itemofTrTd.rank , itemofTrTd.name , itemofTrTd.priceUsd , itemofTrTd.marketCapUsd ,itemofTrTd.vwap24Hr , itemofTrTd.supply , itemofTrTd.volumeUsd24Hr , itemofTrTd.changePercent24Hr);
+    let lowercasedSymbol = (itemofTrTd.symbol).toLowerCase();
+    let renderimg = "https://assets.coincap.io/assets/icons/"+ lowercasedSymbol+ "@2x.png";
+     tableTrTd(itemofTrTd.rank ,itemofTrTd.symbol, renderimg , itemofTrTd.name , itemofTrTd.priceUsd , itemofTrTd.marketCapUsd ,itemofTrTd.vwap24Hr , itemofTrTd.supply , itemofTrTd.volumeUsd24Hr , itemofTrTd.changePercent24Hr);
   }
 
   
